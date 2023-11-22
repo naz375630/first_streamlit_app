57c65f1ed45b468b192a55a7d28acee0ea745213
naz375630 <151690748+naz375630@users.noreply.github.com>
2023-11-23 10:44:09 +1100

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('Hello my first App')
+streamlit.header('This the the header')
+streamlit.text('This is text line 1')
+streamlit.text('This is text line 2')
+streamlit.text('This is text line 3')
